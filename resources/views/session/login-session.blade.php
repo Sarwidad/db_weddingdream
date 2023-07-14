4dff4a4bc0486052f87a3014d61552adac80bef5
Sarwidad <h1101211052@student.untan.ac.id>
2023-07-14 10:23:56 +0700

    login

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -21,20 +21,30 @@
                     @csrf
                     <label>Email :</label>
                     <div class="mb-3">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="admin@softui.com" aria-label="Email" aria-describedby="email-addon">
+                      <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
                       @error('email')
                         <p class="text-danger text-xs mt-2">{{ $message }}</p>
                       @enderror
                     </div>
-                    <label>Password : </label>
+                    <label>Password</label>
                     <div class="mb-3">
-                      <input type="password" class="form-control" name="password" id="password" placeholder="Password" value="secret" aria-label="Password" aria-describedby="password-addon">
+                      <input type="email" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
                       @error('password')
                         <p class="text-danger text-xs mt-2">{{ $message }}</p>
                       @enderror
                     </div>
+                    <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                      <label class="form-check-label" for="rememberMe">Remember me</label>
+                    </div>
+                    <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                <small class="text-muted">Forgot you password? Reset you password 
+                  <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
+                </small>
+                    Don't have an account?
+                    <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
                     <div class="text-center">
-                      <button type="submit" class="btn bg-gradient-dark" style="background-color:E3B463;">Login</button>
+                      <button type="button" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
                     </div>
                   </form>
                 </div>
